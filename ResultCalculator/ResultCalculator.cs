09a57f4ead5ef86f701a6bbc1a2962ccc91a9c54
Mustafiz <hilaly.mustafiz@hotmail.com>
2015-03-06 20:58:14 +0600

    First working version

---
+++ ResultCalculator/ResultCalculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResultCalculator
+{
+    class ResultCalculator
+    {
+        public double PhysicsMarks { set; get; }
+        public double ChemistryMarks { set; get; }
+        public double MathMarks { set; get; }
+
+        public double GetAverageResult()
+        {
+            double average = (PhysicsMarks + ChemistryMarks + MathMarks)/3;
+            return average;
+        }
+
+        public string GetGradeLetter()
+        {
+            double average = GetAverageResult();
+            if (average >= 80)
+            {
+                return "A+";
+            }
+            else if (average >= 70)
+            {
+                return "B+";
+            }
+            else if (average >= 60)
+            {
+                return "C+";
+            }
+            else if (average >= 50)
+            {
+                return "D+";
+            }
+            else
+            {
+                return "F";
+            }
+        }
+    }
+}
---
+++ ResultCalculator/ResultCalculatorUI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ResultCalculator
+{
+    public partial class ResultCalculatorUI : Form
+    {
+        public ResultCalculatorUI()
+        {
+            InitializeComponent();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            ResultCalculator aCalculator = new ResultCalculator();
+            aCalculator.PhysicsMarks = Convert.ToDouble(physicsTextBox.Text);
+            aCalculator.ChemistryMarks = Convert.ToDouble(chemistryTextBox.Text);
+            aCalculator.MathMarks = Convert.ToDouble(mathTextBox.Text);
+
+            averageTextBox.Text = aCalculator.GetAverageResult().ToString();
+            gradeLetterTextBox.Text = aCalculator.GetGradeLetter().ToString();
+        }
+    }
+}
